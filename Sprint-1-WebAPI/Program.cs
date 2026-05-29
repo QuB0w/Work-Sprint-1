18249c49393cae090f3b54162e5087d652e02c1b
QuB0w <QuB0w@users.noreply.github.com>
2026-05-29 18:09:26 +0300

    Sprint 2: Добавлена глобальная обработка ошибок, фильтрация и пагинация
    - Реализован GlobalExceptionHandlingMiddleware для централизованной обработки исключений
    - Добавлена фильтрация мероприятий по title, from, to параметрам
    - Реализована пагинация с PaginatedResult DTO
    - Создан тестовый проект EventService.Tests с 14 тестами
    - Обновлен README.md на русском языке с документацией
    - Все тесты проходят успешно (14/14)
    - Проект собирается и запускается без ошибок

---
+++ Sprint-1-WebAPI/Program.cs
@@ -0,0 +1,17 @@
+using Interfaces;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddScoped<IEventService, EventService>();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+app.UseMiddleware<GlobalExceptionHandlingMiddleware>();
+app.MapControllers();
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.Run();
